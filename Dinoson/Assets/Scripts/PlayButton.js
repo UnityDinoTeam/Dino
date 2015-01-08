df85f31f33a4e6820298d7b91fdd72a2f8cda807
ahu <ahuyalcin06@gmail.com>
2015-01-08 21:30:53 +0200

    setting is ok and play button which is in home scene goes to map scene

--- Dinoson/Assets/Scripts/PlayButton.js
+++ Dinoson/Assets/Scripts/PlayButton.js
@@ -6,5 +6,5 @@ function OnGUI(){
 	GUI.skin = myGuiBlank;
 	
 	if (GUI.Button(Rect(Screen.width/2-100,Screen.height/2-100,200,50),btnTexture))
-		Application.LoadLevel("Scene");
+		Application.LoadLevel("map");
 }
